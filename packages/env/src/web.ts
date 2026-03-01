e5d3321fb74eaf8e24e7a5156521cd619bfb1e7b
Jan Härkönen <jan.harkonen@gmail.com>
2026-03-01 12:23:39 +0200

    fix env variables with self hosted convex setup

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
     VITE_CONVEX_URL: z.url(),
+    VITE_CONVEX_SITE_URL: z.url(),
   },
   runtimeEnv: (import.meta as any).env,
   emptyStringAsUndefined: true,
